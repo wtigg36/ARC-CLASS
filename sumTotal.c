8f83dd92aff783d2cc57cb990b6e01ffe90f8f2d
wtigg36 <William.Tigg@evergreen.edu>
2023-05-29 07:41:00 +0000

    change1

--- sumTotal.c
+++ sumTotal.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int n, char* args[]) {
-    int count;
-    int total = 0; // Initialize total
-    
-    if ((n != 2) || ((count = atoi(args[1])) < 1)) {
-        printf("Usage: ./sumTotal posInt\n");
+int main() {
+    int n, total = 0;
+
+    printf("Enter the initial count (n): ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Invalid input. Initial count should be a non-negative integer.\n");
         return 1;
     }
-    
-    printf("Starting count: %d\n", count);
-    
-    while (count > 0) {
-        total = total + count;
-        count = count - 1;
+
+    printf("Starting count: %d\n", n);
+
+    while (n >= 0) {
+        total += n;
+        n--;
     }
-    
+
     printf("Final total: %d\n", total);
-    
-    return 0; // Add return statement
+
+    return 0;
 }
+
